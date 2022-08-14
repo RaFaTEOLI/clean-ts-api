4373c29d73a3c603dfbb182969939b619e13f704
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-08-14 16:53:09 -0300

    refactor: rename test

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import app from '../config/app';
 
 describe('SignUp Routes', () => {
-  test('should return an account on success', async () => {
+  test('should return 200 on success', async () => {
     await request(app)
       .post('/api/signup')
       .send({
