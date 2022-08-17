902f5deccc8797c7a04d458fcfe8a09fb9c70afd
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-08-17 18:07:17 -0300

    test: ensure LogControllerDecorator will return the same result from controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -47,4 +47,23 @@ describe('LogControllerDecorator', () => {
     await sut.handle(httpRequest);
     expect(handleSpy).toHaveBeenCalledWith(httpRequest);
   });
+
+  test('should return the same result from controller', async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual({
+      body: {
+        name: 'Rafael',
+      },
+      statusCode: 200,
+    });
+  });
 });
