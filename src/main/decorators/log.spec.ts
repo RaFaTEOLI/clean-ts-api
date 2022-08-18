7cf6832ee278d3c3ed9c16100cc8838bc7ef9acc
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-08-18 18:53:05 -0300

    test: ensure LogMongoRepository will persist an error log on success

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -19,7 +19,7 @@ const makeController = (): Controller => {
 
 const makeLogErrorRepository = (): LogErrorRepository => {
   class LogErrorRepositoryStub implements LogErrorRepository {
-    async log(stack: string): Promise<void> {
+    async logError(stack: string): Promise<void> {
       return await Promise.resolve();
     }
   }
@@ -84,7 +84,7 @@ describe('LogControllerDecorator', () => {
   test('should call LogErrorRepository with correct error if controller returns 500', async () => {
     const { sut, controllerStub, logErrorRepositoryStub } = makeSut();
 
-    const logSpy = jest.spyOn(logErrorRepositoryStub, 'log');
+    const logSpy = jest.spyOn(logErrorRepositoryStub, 'logError');
     jest
       .spyOn(controllerStub, 'handle')
       .mockResolvedValueOnce(makeFakeServerError());
