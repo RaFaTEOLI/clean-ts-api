cbaefb33bd2cdf83d878e5b3cf826632e5bf55ee
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-09-05 18:48:02 -0300

    feat: add Authentication to SignUpFactory

--- src/main/factories/signup/signup-factory.ts
+++ src/main/factories/signup/signup-factory.ts
@@ -1,5 +1,8 @@
+import env from '../../config/env';
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account';
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication';
 import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter/bcrypt-adapter';
+import { JWTAdapter } from '../../../infra/cryptography/jwt-adapter/jwt-adapter';
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository';
 import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository';
 import { SignUpController } from '../../../presentation/controllers/signup/signup-controller';
@@ -12,9 +15,18 @@ export const makeSignUpController = (): Controller => {
   const hasher = new BcryptAdapter(salt);
   const addAccountRepository = new AccountMongoRepository();
   const dbAddAccount = new DbAddAccount(hasher, addAccountRepository);
+  const bcryptAdapter = new BcryptAdapter(salt);
+  const jwtAdapter = new JWTAdapter(env.jwtSecret);
+  const dbAuthentication = new DbAuthentication(
+    addAccountRepository,
+    bcryptAdapter,
+    jwtAdapter,
+    addAccountRepository
+  );
   const signUpController = new SignUpController(
     dbAddAccount,
-    makeSignUpValidation()
+    makeSignUpValidation(),
+    dbAuthentication
   );
   const logErrorRepository = new LogMongoRepository();
   return new LogControllerDecorator(signUpController, logErrorRepository);
