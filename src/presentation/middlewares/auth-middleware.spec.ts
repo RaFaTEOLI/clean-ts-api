aae10c1a519ea799ea9dfcd7e386f05159ce1026
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-10-20 18:34:02 +0200

    test: ensure AuthMiddleware will return 403 if LoadAccountByToken returns null

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -48,7 +48,6 @@ describe('AuthMiddleware', () => {
   test('should return 403 if no x-access-token exists in headers', async () => {
     const { sut } = makeSut();
     const httpResponse = await sut.handle({});
-
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()));
   });
 
@@ -56,7 +55,13 @@ describe('AuthMiddleware', () => {
     const { sut, loadAccountByTokenStub } = makeSut();
     const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load');
     await sut.handle(makeFakeRequest());
-
     expect(loadSpy).toHaveBeenCalledWith('any_token');
+  });
+
+  test('should return 403 if LoadAccountByToken returns null', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut();
+    jest.spyOn(loadAccountByTokenStub, 'load').mockResolvedValue(null);
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(forbidden(new AccessDeniedError()));
   });
 });
