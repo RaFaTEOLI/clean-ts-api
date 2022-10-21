74f5f27b9e3a7619eb15efc29cef35a56b684df0
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-10-21 10:39:31 +0200

    feat: ensure AccountMongoRepository will return an account on loadByToken without role

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -3,15 +3,18 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/account/add-account-repository';
 import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/account/load-account-by-email-repository';
 import { UpdateAccessTokenRepository } from '../../../../data/protocols/db/account/update-access-token-repository';
+import { LoadAccountByTokenRepository } from '../../../../data/usecases/load-account-by-token/db-load-account-by-token-protocols';
 import { AccountModel } from '../../../../domain/models/account';
 import { AddAccountModel } from '../../../../domain/usecases/add-account';
 import { MongoHelper } from '../helpers/mongo-helper';
 import { ObjectId } from 'mongodb';
+
 export class AccountMongoRepository
   implements
     AddAccountRepository,
     LoadAccountByEmailRepository,
-    UpdateAccessTokenRepository
+    UpdateAccessTokenRepository,
+    LoadAccountByTokenRepository
 {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts');
@@ -37,5 +40,17 @@ export class AccountMongoRepository
         },
       }
     );
+  }
+
+  async loadByToken(
+    token: string,
+    role?: string
+  ): Promise<AccountModel | null> {
+    const accountCollection = await MongoHelper.getCollection('accounts');
+    const account = await accountCollection.findOne({
+      accessToken: token,
+      role,
+    });
+    return account ? MongoHelper.format(account) : null;
   }
 }
