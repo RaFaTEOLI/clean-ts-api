0e7bc107450b809c868ef84d33937062f4dd6b5d
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-10-21 10:00:30 +0200

    test: ensure JwtAdapter will call verify with correct values

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -5,6 +5,10 @@ jest.mock('jsonwebtoken', () => ({
   async sign(): Promise<string> {
     return await Promise.resolve('any_token');
   },
+
+  async verify(): Promise<string> {
+    return await Promise.resolve('any_value');
+  },
 }));
 
 const makeSut = (): JWTAdapter => {
@@ -31,6 +35,15 @@ describe('JWT Adapter', () => {
       jest.spyOn(jwt, 'sign').mockRejectedValueOnce(new Error() as never);
       const promise = sut.encrypt('any_id');
       await expect(promise).rejects.toThrow();
+    });
+  });
+
+  describe('verify()', () => {
+    test('should call verify with correct values', async () => {
+      const sut = makeSut();
+      const verifySpy = jest.spyOn(jwt, 'verify');
+      await sut.decrypt('any_token');
+      expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret');
     });
   });
 });
