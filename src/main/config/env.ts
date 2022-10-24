3b5a72c1893bb1af7c63a049e76b19fafb26ebce
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-10-24 18:16:46 +0200

    fix: update mapping within routes to use debug in docker

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://root:1234@mongo:27017/',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://root:1234@localhost:27017/',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'f6h!u#98jp',
 };
