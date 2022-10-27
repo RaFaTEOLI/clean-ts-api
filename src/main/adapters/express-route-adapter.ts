d7caa3688244beaed93f2d82c2a724d8e8b82c09
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-10-27 12:20:21 +0200

    feat: ensure SurveyResult routes will return 200 on save survey result with accessToken

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -5,14 +5,14 @@ export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
     const httpRequest: HttpRequest = {
       body: req.body,
+      params: req.params,
+      account: req.account
     };
     const httpResponse = await controller.handle(httpRequest);
     if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
       res.status(httpResponse.statusCode).json(httpResponse.body);
     } else {
-      res
-        .status(httpResponse.statusCode)
-        .json({ error: httpResponse.body.message });
+      res.status(httpResponse.statusCode).json({ error: httpResponse.body.message });
     }
   };
 };
