1caec38eac90e9dc0f1b64b0040a4ad558787032
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-10-27 12:53:10 +0200

    feat: ensure an account will be in the logged user request

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,29 +1,19 @@
-import {
-  LoadAccountByToken,
-  Middleware,
-  HttpRequest,
-  HttpResponse,
-} from './auth-middleware-protocols';
+import { LoadAccountByToken, Middleware, HttpRequest, HttpResponse } from './auth-middleware-protocols';
 import { AccessDeniedError } from '../errors';
 import { forbidden, serverError, success } from '../helpers/http/http-helper';
 
 export class AuthMiddleware implements Middleware {
-  constructor(
-    private readonly loadAccountByToken: LoadAccountByToken,
-    private readonly role?: string
-  ) {}
+  constructor(private readonly loadAccountByToken: LoadAccountByToken, private readonly role?: string) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const accessToken = httpRequest.headers?.['x-access-token'];
 
       if (accessToken) {
-        const account = await this.loadAccountByToken.load(
-          accessToken,
-          this.role
-        );
+        const account = await this.loadAccountByToken.load(accessToken, this.role);
         if (account) {
-          return success(account);
+          const { password, ...accountToReturn } = account;
+          return success({ account: accountToReturn });
         }
       }
 
