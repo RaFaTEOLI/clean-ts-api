b4ed59838a7b3178766280ba9c2a66b8a6d91145
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-10-28 10:16:24 +0200

    feat: add api docs for login

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,28 @@
+import { loginPath } from './paths';
+import { accountSchema, loginParamsSchema } from './schemas';
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean TS API',
     description: '🧼 NodeJs API with Typescript, TDD, Clean Architecture, Design Patterns and SOLID Principles',
     version: '1.0.0'
+  },
+  servers: [
+    {
+      url: '/api'
+    }
+  ],
+  tags: [
+    {
+      name: 'Login'
+    }
+  ],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 };
