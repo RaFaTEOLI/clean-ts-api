ced2a91b1eb2932e39d1202935cd7edf92334e2c
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-10-28 09:25:15 +0200

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean TS API',
+    description: '🧼 NodeJs API with Typescript, TDD, Clean Architecture, Design Patterns and SOLID Principles',
+    version: '1.0.0'
+  }
+};
