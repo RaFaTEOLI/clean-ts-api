c57ef907172e659ca1e89b75ca74ed1f0bff0cb0
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-10-28 10:51:51 +0200

    docs: add surveys api docs

---
+++ src/main/docs/paths/survey-path.ts
@@ -0,0 +1,64 @@
+export const surveyPath = {
+  get: {
+    security: [
+      {
+        apiKeyAuth: []
+      }
+    ],
+    tags: ['Survey'],
+    summary: 'API to list all surveys',
+    description: 'This route is only for **authenticated users**',
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveys'
+            }
+          }
+        }
+      },
+      204: {
+        description: 'Success, No Content'
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  },
+  post: {
+    security: [
+      {
+        apiKeyAuth: []
+      }
+    ],
+    tags: ['Survey'],
+    summary: 'API to create a survey',
+    description: 'This route is only for **admins**',
+    requestBody: {
+      required: true,
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/addSurveyParams'
+          }
+        }
+      }
+    },
+    responses: {
+      204: {
+        description: 'Success, No Content'
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  }
+};
