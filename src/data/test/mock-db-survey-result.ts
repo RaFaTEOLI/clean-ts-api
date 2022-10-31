b0febd812f29c18c2b91b5656c418e3246670e10
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-10-31 16:00:59 +0100

    refactor: move mockLoadSurveyResultRepository into a separate file

--- src/data/test/mock-db-survey-result.ts
+++ src/data/test/mock-db-survey-result.ts
@@ -1,7 +1,8 @@
-import { SaveSurveyResultRepository } from '../protocols/db/survey-result/save-survey-result-repository';
+import { SaveSurveyResultRepository } from '@/data/protocols/db/survey-result/save-survey-result-repository';
+import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository';
 import { SaveSurveyResultParams } from '@/domain/usecases/survey-result/save-survey-result';
+import { SurveyResultModel } from '@/domain/models/survey-result';
 import { mockSurveyResultModel } from '@/domain/test';
-import { SurveyResultModel } from '@/data/usecases/survey/load-survey-by-id/db-load-survey-by-id-protocols';
 
 export const mockSaveSurveyResultRepository = (): SaveSurveyResultRepository => {
   class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
@@ -10,4 +11,13 @@ export const mockSaveSurveyResultRepository = (): SaveSurveyResultRepository => 
     }
   }
   return new SaveSurveyResultRepositoryStub();
+};
+
+export const mockLoadSurveyResultRepository = (): LoadSurveyResultRepository => {
+  class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
+    async loadBySurveyId(surveyId: string): Promise<SurveyResultModel> {
+      return await Promise.resolve(mockSurveyResultModel());
+    }
+  }
+  return new LoadSurveyResultRepositoryStub();
 };
